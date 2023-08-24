06f44ec6e66185a6d3b55486dd30d55395e88e49
j.jauregui@uniandes.edu.co <j.jauregui@uniandes.edu.co>
2023-08-24 13:49:25 -0500

    Inicio

---
+++ main.py
@@ -0,0 +1,10 @@
+# Proyecto 0 Lenguajes y Máquinas - Grupo 12
+
+#Integrantes:
+#   - Juan Manuel Jáuregui Rozo - 201922481
+#   - Adrián Suarez 
+
+# Lectura de archivo que se requiere verificar.
+archivo = open('ejemplo.txt', 'r')
+
+# Palabras reservadas del lenguaje.
