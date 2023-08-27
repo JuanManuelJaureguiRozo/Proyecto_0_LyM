e0b4a15ec6c93595322eeee389a2320fb37812dc
Adrián Suárez <a.suarezg@uniandes.edu.co>
2023-08-27 08:44:34 -0500

    a

--- main.py
+++ main.py
@@ -25,7 +25,27 @@ LISTA DE DUDAS QUE VAN SURGIENDO
 - monitor dijo que había una línea especial con la que empezaba; no la veo en los ejemplos; omitir de momento
 - Cuando lee los números en el txt, ¿los toma como str?
 - ¿Siempre, después de un método, hay una "{" sola, o puede que haya código despues, tipo "{ while...."?
-- ¿hay algún caso en que se definan variables después de métodos, o incluso dentro? ¿O siempre al principio?     
+- ¿hay algún caso en que se definan variables después de métodos, o incluso dentro? ¿O siempre al principio?   
+- los métodos siempre tienen un espacio para los paréntesis? Tipo "jump ()"  
+
+LISTA PROBLEMAS / DETALLES QUE FALTAN POR IMPLEMENTAR
+- Seguramente toque separar los parametros str / int para poder verficar dentro de un método que sea del tipo correcto
+    - Cuando se declare un nuevo parametro toca intentara volverlo int con un try/except, así sabemos si es int o no, pues
+    pues todos vienen como str
+- Seguramente toque separar los métodos normales, los nuevos, y los condicionales, haciendo una nueva función para verificar los condicionales
+    - Puede hacerse agregando un 5to caso, o con un if dentro del verificador de métodos; seguramente 1era opción
+- Falta, después del test_Var, decirle que lo agregue a la lista
+- Falta limpiar bien la linea, pues cuando vienen pegados toca separar los paréntesis, los ";", los "\t", y otros casos posibles
+    - La última línea de un bloque NO tiene ";" lo cual será un problema
+        - tal vez verificando que la siguiente línea sea "}" se pueda saltar este caso
+- Para los "{}" mejor hacer una pila simple; en una lista previamente declarada se van agregando los "{", y cada que salga un 
+"}" se va eliminando la -1 posición con una función, la cual preguna el len de la lista. Si es <0 se devuelve True y el programa
+sigue, sino se para, y al final del todo se pregunta el tamaño de la lista, si no es =0, está mal y el txt es incorrecto
+- Para leer las líneas sería mejor hacer un for, donde al final se sume 1 a la línea, así podemos seguir leyendo en casos correctos
+- Recordar que todo debe volverse minúscula, sobre todo en los parámetros
+- Los nuevos métodos toca agregarlos de una en la lista de métodos para que haya recursión, pero ¿cómo hacemos para saber si 
+los parámetros son correctos (tipo), antes de leer las líneas de abajo?
+
     
 """
 
@@ -37,6 +57,8 @@ LISTA DE DUDAS QUE VAN SURGIENDO
 # Crear función para agregar un método, así se puede hacer recursión
 lista_noms_metodos = ["jump", "walk", "leap", "turn", "turnto",
                       "drop", "grab", "letGo", "nop", "if", "else", "while", "repeat"]
+
+lista_condicionales =["facing", "can", "not"]
 
 # parametros por default del proyecto; crear función que agregue cuando se declare una nueva variable
 ## Recordar que toca tomar volver todo a minúscula antes de corroborarlo con estas variables
@@ -71,8 +93,43 @@ def test_Proc (list: linea):
 def test_metodo (list:linea):
     met = linea[0]
     
-    if met == 
+    if met == "jump":
+        print(a)
     
+    elif met == "walk":
+        print(a)
+        
+    elif met == "leap":
+        print(a)
+        
+    elif met == "turn":
+        print(a)
+        
+    elif met == "turnto":
+        print(a)
+        
+    elif met == "drop":
+        print(a)
+        
+    elif met == "grab":
+        print(a)
+        
+    elif met == "letGo":
+        print(a)   
+    
+    elif met == "nop":
+        print(a)
+        
+    elif met == "if":
+        print(a)
+        
+    elif met == "while":
+        print(a)
+        
+    elif met == "repeat":
+        print(a)    
+     
+
 
 prueba = limpiar_linea(linea)
 
