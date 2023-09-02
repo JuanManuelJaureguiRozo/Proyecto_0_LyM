c4803983785f665817ced018dde05efe9a98cc10
Adrián Suárez <a.suarezg@uniandes.edu.co>
2023-09-02 12:27:02 -0500

    Edición sin 2 líneas

--- prueba.py
+++ prueba.py
@@ -1,3 +1,27 @@
+import modelo as modelo
+archivo = open("prueba.txt", "r")
 
-lista = [1,2,3]
-print(lista)
+#print(archivo[0])
+
+with open('prueba.txt', 'r') as archivo:
+    # Crea un iterador a partir del archivo
+    lineas = iter(archivo)
+    
+    # Itera a través de las líneas del archivo
+    for ln in lineas:
+        # Procesa la línea actual
+        linea1 = modelo.limpiar_linea(ln)
+        print("Línea actual:", linea1)
+        
+        try:
+            linea2 = modelo.limpiar_linea(next(lineas))
+        except StopIteration:
+            pass
+        
+        print(linea2)
+        
+        
+
+
+# linea1 es la del for
+# linea2 es "next(lineas)"
