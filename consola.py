d2cd43dd8e04074e90a1004be5e1a6ff6e1a05d7
j.jauregui@uniandes.edu.co <j.jauregui@uniandes.edu.co>
2023-09-04 16:38:55 -0500

    Avances

--- consola.py
+++ consola.py
@@ -4,6 +4,8 @@ archivo = open("prueba.txt", "r")
 
 corchetes =[]
 
+params_proc_actual = []
+
 matriz_archivo = []
 
 for linea in archivo:
@@ -11,68 +13,86 @@ for linea in archivo:
         linea = modelo.limpiar_linea(linea)
         matriz_archivo.append(linea)
 
-def funcion_consola(linea_en_una_lista, matriz_archivo, numero):
-    
-    try: 
+def funcion_consola(matriz_archivo):
+    centinela = True
+    tam = len(matriz_archivo)
+    i = 0
+    while centinela == True and i < tam:
+        linea_en_una_lista = matriz_archivo[i] 
+        print(linea_en_una_lista)
+        numero = matriz_archivo.index(linea_en_una_lista)
 
         if linea_en_una_lista[0] == "defVar":
             
             marca = modelo.test_Var(linea_en_una_lista)
 
-            if marca == True:
-                pass
+            if marca == False or marca == None:
+                centinela = False
+                print(marca)
+            
+            else:
+                i += 1
             
         elif linea_en_una_lista[0] == "defProc" or linea_en_una_lista[0] in modelo.lista_noms_proc: 
 
             params = modelo.almacenar_parametros(linea_en_una_lista)
+            for param in params:
+                params_proc_actual.append(param)
             marca = modelo.test_Proc(linea_en_una_lista, params)
 
-            if marca == True:
-                pass
+            if marca == False or marca == None:
+                centinela = False
+                print(marca)
+            
+            else:
+                i += 1
             
         elif (linea_en_una_lista[0] == "{") or (linea_en_una_lista[0] == "}" ): 
             
             marca = modelo.test_corchete(linea_en_una_lista)
 
-            if marca == True:
-                pass
+            if marca == False or marca == None:
+                centinela = False
+                print(marca)
             
-            elif marca == True:
-                if linea_en_una_lista[0] == "{":
-                    corchetes.append("{")
-                    
-                elif linea_en_una_lista[0] == "}":
-                    try:
-                        corchetes.pop(-1)
-                    except: print("False")
+            else:
+                i += 1
                      
         elif (linea_en_una_lista[0] in modelo.lista_noms_metodos):
             
-            marca = modelo.test_metodo(linea_en_una_lista, matriz_archivo, numero)
+            params = modelo.almacenar_parametros(linea_en_una_lista)
+            if params != None:
+                for v in params:
+                    if v in params_proc_actual:
+                        marca = modelo.test_metodo(linea_en_una_lista, matriz_archivo, numero, params)
+            else:
+                marca = modelo.test_metodo(linea_en_una_lista, matriz_archivo, numero, params)
 
-            if marca == True:
-                pass
+            if marca == False or marca == None:
+                centinela = False
+                print(marca)
+            
+            else:
+                i += 1
 
         elif linea_en_una_lista[0] in modelo.lista_condicionales:
-            
+
             for elemento in linea_en_una_lista:
                 if elemento == "{":
             
-                    marca = modelo.test_Proc(linea_en_una_lista, matriz_archivo, numero)
-                    #print(marca)
+                    marca = modelo.test_Proc(linea_en_una_lista, numero)
 
-            if marca == True:
-                pass
+            if marca == False or marca == None:
+                centinela = False
+                print(marca)
+            
+            else:
+                i += 1
 
-    except: print("False")
+    if centinela == True:
+        print("El programa es correcto")
 
-for linea_en_una_lista in matriz_archivo:
-    try:
-        print(linea_en_una_lista)
-        numero = matriz_archivo.index(linea_en_una_lista)
-        funcion_consola(linea_en_una_lista, matriz_archivo, numero)
-
-    except: print("False")
+funcion_consola(matriz_archivo)
 
 
 
